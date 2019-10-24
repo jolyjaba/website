52f7f07e2f865e917b33bf5a308b790957df9861
Daryn <daryn07102001@gmail.com>
2019-10-25 00:06:34 +0600

    S

--- week6/Task5_1/script.js
+++ week6/Task5_1/script.js
@@ -49,45 +49,48 @@ function show() {
 }
 
 function add() {
-    let tbody = document.querySelector("tbody");
-    let newRow = document.createElement("tr");
-    let firstCell = document.createElement("td");
-    let secondCell = document.createElement("td");
-    let thirdCell = document.createElement("td");
-    let fourthCell = document.createElement("td");
-    let fifthCell = document.createElement("td");
-    let sixthCell = document.createElement("td");
-    let seventhCell = document.createElement("td");
-    let accountStatus = (document.querySelector("#select_buttons input").checked == true) ? "Active" : "Inactive";
-    firstCell.innerHTML = `<label class='container'><span>${accountStatus}</span><input type='checkbox'><span class='checkmark'></span></label>`;
-    secondCell.innerHTML = document.querySelector("#input_name").value;
-    thirdCell.innerHTML = document.querySelector("#input_email").value;
-    fourthCell.innerHTML = document.querySelector("#select_option").value;
-    fifthCell.innerHTML = document.querySelector("#input_phone").value;
-    seventhCell.innerHTML = `<img class='view' src='view-image.png' alt='view'><img onclick='showDel(${index})' class='delete' src='Delete-Icon.svg' alt='Delete-Icon'><img onclick='edit(${index})' class='edit' src='Edit-Icon.svg' alt='Edit-Icon'>`;
+    alert(`Index of added row: ${index}`);
+    let tr = document.createElement("tr");
+    tr.innerHTML =
+        `<tr>
+            <td>
+                <label class="container">
+                    <span>${(document.querySelector("#select_buttons input").checked == true) ? "Active" : "Inactive"}</span>
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                </label>
+            </td>
+            <td>${document.querySelector("#input_name").value}</td>
+            <td>${document.querySelector("#input_email").value}</td>
+            <td>${document.querySelector("#select_option").value}</td>
+            <td>${document.querySelector("#input_phone").value}</td>
+            <td></td>
+            <td>
+                <img class="view" src="view-image.png" alt="view">
+                <img onclick="showDelete(${index})" class="delete" src="Delete-Icon.svg" alt="Delete-Icon">
+                <img onclick="showEdit(${index})" class="edit" src="Edit-Icon.svg" alt="Edit-Icon">
+            </td>
+        </tr>`;
+    document.querySelector("tbody").appendChild(tr);
     index++;
-    newRow.appendChild(firstCell);
-    newRow.appendChild(secondCell);
-    newRow.appendChild(thirdCell);
-    newRow.appendChild(fourthCell);
-    newRow.appendChild(fifthCell);
-    newRow.appendChild(sixthCell);
-    newRow.appendChild(seventhCell);
-    tbody.appendChild(newRow);
     hide();
 }
 
-function showDel(i) {
+function showDelete(i) {
+    alert(`Index of showed row: ${i}`);
     document.querySelector("#deleteWindow").style.display = "block";
     document.querySelector("#main_del_win").style.display = "block";
     document.querySelector("#main_del_win img:nth-child(3)").addEventListener("click", function () {
+        alert(`Index of deleted row: ${i}`);
         document.querySelector("tbody").deleteRow(i);
         hide();
         index--;
     });
 }
 
-function edit(i) {
+function showEdit(i) {
+    alert(`Index of showed row: ${i}`);
+    document.querySelector("#edit_select_buttons input").checked = (document.querySelector("tbody").rows[i].cells[0].querySelector("span").innerHTML == "Active") ? true : false;
     document.querySelector("#edit_input_name").value = document.querySelector("tbody").rows[i].cells[1].innerHTML;
     document.querySelector("#edit_input_email").value = document.querySelector("tbody").rows[i].cells[2].innerHTML;
     document.querySelector("#edit_input_phone").value = document.querySelector("tbody").rows[i].cells[4].innerHTML;
@@ -95,12 +98,21 @@ function edit(i) {
     document.getElementById("overlay").style.display = "block";
     document.getElementById("edit_win").style.display = "block";
     document.querySelector("#edit_select_buttons img").addEventListener("click", function () {
-        let accountStatus = (document.querySelector("#edit_select_buttons input").checked == true) ? "Active" : "Inactive";
-        document.querySelector("tbody").rows[i].cells[0].innerHTML = `<label class='container'><span>${accountStatus}</span><input type='checkbox'><span class='checkmark'></span></label>`;
-        document.querySelector("tbody").rows[i].cells[1].innerHTML = document.querySelector("#edit_input_name").value;
-        document.querySelector("tbody").rows[i].cells[2].innerHTML = document.querySelector("#edit_input_email").value;
-        document.querySelector("tbody").rows[i].cells[3].innerHTML = document.querySelector("#edit_select_option").value;
-        document.querySelector("tbody").rows[i].cells[4].innerHTML = document.querySelector("#edit_input_phone").value;
-        hide();
+        saveEdit(i);
     });
+}
+
+function saveEdit(i) {
+    alert(`Index of edited row: ${i}`);
+    document.querySelector("tbody").rows[i].cells[0].innerHTML = 
+    `<label class='container'>
+        <span>${(document.querySelector("#edit_select_buttons input").checked == true) ? "Active" : "Inactive"}</span>
+        <input type='checkbox'>
+        <span class='checkmark'></span>
+    </label>`;
+    document.querySelector("tbody").rows[i].cells[1].innerHTML = document.querySelector("#edit_input_name").value;
+    document.querySelector("tbody").rows[i].cells[2].innerHTML = document.querySelector("#edit_input_email").value;
+    document.querySelector("tbody").rows[i].cells[3].innerHTML = document.querySelector("#edit_select_option").value;
+    document.querySelector("tbody").rows[i].cells[4].innerHTML = document.querySelector("#edit_input_phone").value;
+    hide();
 }
