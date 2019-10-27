d0800bf05f25615845f9fa81ecf8b810bc4186d6
Daryn <daryn07102001@gmail.com>
2019-10-27 20:30:46 +0600

    S

---
+++ week7/script2.js
@@ -0,0 +1,26 @@
+let countries = ["Kazakhstan", "Russia", "England", "France"];
+let cities_by_country = {
+    "Kazakhstan": ["Almaty", "Astana", "Karagandy"],
+    "Russia": ["Moscow", "Saint-Petersburg", "Kazan"],
+    "England": ["London", "Manchester", "Liverpool"],
+    "France": ["Paris", "Lyon", "Marseille"]
+};
+let selectCountries = document.getElementById("countries");
+let selectCities = document.getElementById("cities");
+
+function show() {
+    for (let country of countries) {
+        let countryOption = document.createElement("option");
+        countryOption.innerHTML = country;
+        selectCountries.appendChild(countryOption);
+    }
+    selectCountries.onchange = function () {
+        selectCities.length = 1;
+        for (let city of cities_by_country[this.value]) {
+            let cityOption = document.createElement("option");
+            cityOption.innerHTML = city;
+            selectCities.appendChild(cityOption);
+        }
+    }
+}
+show();
