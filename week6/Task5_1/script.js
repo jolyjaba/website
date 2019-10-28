01b1e5d86c7f6b855b1df97bf8a788c6a1e61c28
Daryn <daryn07102001@gmail.com>
2019-10-28 18:33:03 +0600

    S

--- week6/Task5_1/script.js
+++ week6/Task5_1/script.js
@@ -1,5 +1,3 @@
-let index = 0;
-
 function reset() {
     document.querySelector("#input_name").value = "";
     document.querySelector("#input_email").value = "";
@@ -23,13 +21,12 @@ function show() {
 }
 
 function add() {
-    alert(`Index of added row: ${index}`);
     let tr = document.createElement("tr");
     tr.innerHTML =
     `<tr>
         <td>
             <label class="container">
-                <span>${(document.querySelector("#select_buttons input").checked == true) ? "Active" : "Inactive"}</span>
+                <span>${document.querySelector("#select_buttons input").checked == true ? "Active" : "Inactive"}</span>
                 <input type="checkbox">
                 <span class="checkmark"></span>
             </label>
@@ -41,49 +38,45 @@ function add() {
         <td></td>
         <td>
             <img class="view" src="view-image.png" alt="view">
-            <img onclick="showDelete(${index})" class="delete" src="Delete-Icon.svg" alt="Delete-Icon">
-            <img onclick="showEdit(${index})" class="edit" src="Edit-Icon.svg" alt="Edit-Icon">
+            <img onclick="showDelete(this)" class="delete" src="Delete-Icon.svg" alt="Delete-Icon">
+            <img onclick="showEdit(this)" class="edit" src="Edit-Icon.svg" alt="Edit-Icon">
         </td>
     </tr>`;
     document.querySelector("tbody").appendChild(tr);
-    index++;
     hide();
 }
 
-function showDelete(i) {
+function showDelete(thisRow) {
     document.querySelector("#deleteWindow").style.display = "block";
     document.querySelector("#main_del_win").style.display = "block";
     document.querySelector("#main_del_win img:nth-child(3)").onclick = function () {
-        document.querySelector("tbody").deleteRow(i);
+        document.querySelector("tbody").deleteRow(thisRow.parentElement.parentElement.rowIndex - 1);
         hide();
-        index--;
     };
 }
-
-function showEdit(i) {
-    // let tr = document.querySelector("tbody").rows[i];
-    // document.querySelector("#edit_select_buttons input").checked = (tr.cells[0].querySelector("span").innerHTML == "Active") ? true : false;
-    // document.querySelector("#edit_input_name").value = tr.cells[1].innerHTML;
-    // document.querySelector("#edit_input_email").value = tr.cells[2].innerHTML;
-    // document.querySelector("#edit_input_phone").value = tr.cells[4].innerHTML;
-    // document.querySelector("#edit_select_option").value = tr.cells[3].innerHTML;
+let tr;
+function showEdit(thisRow) {
+    tr = thisRow.parentElement.parentElement;
+    document.querySelector("#edit_select_buttons input").checked = tr.cells[0].querySelector("span").innerHTML == "Active" ? true : false;
+    document.querySelector("#edit_input_name").value = tr.cells[1].innerHTML;
+    document.querySelector("#edit_input_email").value = tr.cells[2].innerHTML;
+    document.querySelector("#edit_input_phone").value = tr.cells[4].innerHTML;
+    document.querySelector("#edit_select_option").value = tr.cells[3].innerHTML;
     document.getElementById("overlay").style.display = "block";
     document.getElementById("edit_win").style.display = "block";
-    
-    document.querySelector("#edit_select_buttons img").addEventListener("click", function () {
-        alert(`Index of edited row: ${i}`);
-        tr.cells[0].innerHTML = `
-        <label class='container'>
-            <span>${(document.querySelector("#edit_select_buttons input").checked == true) ? "Active" : "Inactive"}</span>
-            <input type='checkbox'>
-            <span class='checkmark'></span>
-        </label>
-        `;
-        tr.cells[1].innerHTML = document.querySelector("#edit_input_name").value;
-        tr.cells[2].innerHTML = document.querySelector("#edit_input_email").value;
-        tr.cells[3].innerHTML = document.querySelector("#edit_select_option").value;
-        tr.cells[4].innerHTML = document.querySelector("#edit_input_phone").value;
-        hide();
-    });
 }
-showEdit();
+
+function saveEdit() {
+    tr.cells[0].innerHTML = `
+    <label class='container'>
+        <span>${document.querySelector("#edit_select_buttons input").checked == true ? "Active" : "Inactive"}</span>
+        <input type='checkbox'>
+        <span class='checkmark'></span>
+    </label>
+    `;
+    tr.cells[1].innerHTML = document.querySelector("#edit_input_name").value;
+    tr.cells[2].innerHTML = document.querySelector("#edit_input_email").value;
+    tr.cells[3].innerHTML = document.querySelector("#edit_select_option").value;
+    tr.cells[4].innerHTML = document.querySelector("#edit_input_phone").value;
+    hide();
+}
