3f32f2ddc0d63b115d4966aabdf398a45e81e83d
Daryn <daryn07102001@gmail.com>
2019-10-30 16:24:12 +0600

    S

--- week6/Task5_2/script.js
+++ week6/Task5_2/script.js
@@ -9,31 +9,37 @@ let area = document.getElementById("Frame"),
     width = document.getElementById("Width"),
     height = document.getElementById("Height"),
     css_border = document.getElementById("css-border");
-window.onload = function () {
-    document.getElementById("tools").onclick = function () {
-        document.getElementById("frame").style.display = "none";
-        document.getElementById("fill").style.display = "block";
-    }
-    document.getElementById("frame_prop").onclick = function () {
-        document.getElementById("fill").style.display = "none";
-        document.getElementById("frame").style.display = "block";
-    }
-    document.getElementById("add_object").onclick = function () {
-        area.querySelector("span").style.display = "none";
-        let newImage = document.createElement("img");
-        newImage.width = "200";
-        newImage.src = `${object.value}.svg`;
-        newImage.style.position = position_type.value;
-        newImage.style.marginTop = position_x.value;
-        newImage.style.marginRight = position_y.value;
-        newImage.style = css_obj.value;
-        area.appendChild(newImage);
-    }
-    document.getElementById("save_frame").onclick = function () {
-        area.style.borderRadius = border_radius.value;
-        area.style.background = background.value;
-        area.style.width = width.value;
-        area.style.height = height.value;
-        area.style = css_border.value;
-    }
-}
+
+document.getElementById("save_frame").onclick = function () {
+    area.style.borderRadius = border_radius.value;
+    area.style.background = background.value;
+    area.style.width = width.value;
+    area.style.height = height.value;
+    area.style = css_border.value;
+};
+
+document.getElementById("add_object").onclick = function () {
+    area.querySelector("span").style.display = "none";
+    let newImage = document.createElement("img");
+    newImage.width = "200";
+    newImage.src = `${object.value}.svg`;
+    newImage.style.position = position_type.value;
+    newImage.style.marginTop = position_x.value;
+    newImage.style.marginRight = position_y.value;
+    newImage.style = css_obj.value;
+    area.appendChild(newImage);
+};
+
+document.getElementById("tools").onclick = function () {
+    document.getElementById("frame_prop").style.borderBottom = "none";
+    document.getElementById("frame").style.display = "none";
+    document.getElementById("tools").style.borderBottom = "5px solid #8954BA";
+    document.getElementById("fill").style.display = "block";
+};
+
+document.getElementById("frame_prop").onclick = function () {
+    document.getElementById("tools").style.borderBottom = "none";
+    document.getElementById("fill").style.display = "none";
+    document.getElementById("frame_prop").style.borderBottom = "5px solid #8954BA";
+    document.getElementById("frame").style.display = "block";
+};
