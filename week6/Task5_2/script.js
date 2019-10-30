cc53c937fe1df6138ce914808c31bc9219632467
Daryn <daryn07102001@gmail.com>
2019-10-30 16:52:18 +0600

    Update script.js

--- week6/Task5_2/script.js
+++ week6/Task5_2/script.js
@@ -9,7 +9,6 @@ let area = document.getElementById("Frame"),
     width = document.getElementById("Width"),
     height = document.getElementById("Height"),
     css_border = document.getElementById("css-border");
-
 document.getElementById("save_frame").onclick = function () {
     area.style.borderRadius = border_radius.value;
     area.style.background = background.value;
@@ -17,26 +16,22 @@ document.getElementById("save_frame").onclick = function () {
     area.style.height = height.value;
     area.style = css_border.value;
 };
-
 document.getElementById("add_object").onclick = function () {
     area.querySelector("span").style.display = "none";
     let newImage = document.createElement("img");
     newImage.width = "200";
     newImage.src = `${object.value}.svg`;
     newImage.style.position = position_type.value;
-    newImage.style.marginTop = position_x.value;
-    newImage.style.marginRight = position_y.value;
-    newImage.style = css_obj.value;
+    newImage.style.top = position_x.value + 'px';
+    newImage.style.left = position_y.value + 'px';
     area.appendChild(newImage);
 };
-
 document.getElementById("tools").onclick = function () {
     document.getElementById("frame_prop").style.borderBottom = "none";
     document.getElementById("frame").style.display = "none";
     document.getElementById("tools").style.borderBottom = "5px solid #8954BA";
     document.getElementById("fill").style.display = "block";
 };
-
 document.getElementById("frame_prop").onclick = function () {
     document.getElementById("tools").style.borderBottom = "none";
     document.getElementById("fill").style.display = "none";
